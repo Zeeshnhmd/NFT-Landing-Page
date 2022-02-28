5ebde2acf7758cad315ac6d584f7f96a5bbf5366
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-02-28 12:57:27 +0530

    add clients section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-
+import clients1 from '../assets/clients1.png';
+import clients2 from '../assets/clients2.png';
+import clients3 from '../assets/clients3.png';
+import clients4 from '../assets/clients4.png';
+import clients5 from '../assets/clients5.png';
 const Clients = () => {
-	return <div>Clients</div>;
+	const data = [clients1, clients2, clients3, clients4, clients5];
+	return (
+		<div className="clients">
+			<div className="container">
+				<div className="clients-container">
+					{data.map((client, index) => (
+						<div className="client" key={index}>
+							<img src={client} alt="clients" />
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Clients;
